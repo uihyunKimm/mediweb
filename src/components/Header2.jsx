dbb7ff582ecb4866fb52a0ba14b21e406f8afc74
DESKTOP-B2E7B3M\asmin <dmlxld@gmail.com>
2024-09-06 17:20:35 +0900

    add add logo image/ fix side bar animation commit

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Logo from '../image/logo.png'
 
 const menuItems = [
   {
@@ -42,7 +43,7 @@ const menuItems = [
 const NavButton = ({ active, onClick }) => {
   return (
     <div
-      className={`absolute top-1/2 right-52 z-50 -translate-y-1/2 w-12 h-7 cursor-pointer transition-all duration-300 ease-in-out max-xl:right-12 max-md:hidden
+      className={`absolute top-1/2 right-52 z-50 -translate-y-1/2 w-12 h-7 cursor-pointer transition-all duration-300 ease-in-out max-xl:right-6 
         before:bg-white before:block before:absolute before:left-0 before:top-1 before:z-10 before:w-full before:h-0.5 before:transition-width before:duration-500 before:delay-75
         after:top-4 after:bg-white after:block after:absolute after:left-0 after:z-10 after:w-full after:h-0.5 after:transition-width after:duration-500 after:delay-75
         ${active ? 'before:right-0 before:top-1/2 before:rotate-45 after:top-1/2 after:-rotate-45' : ''}`}
@@ -86,17 +87,18 @@ const NavMenu = ({ active }) => {
         <div className={`right-0 w-0 h-1/4 absolute transition-[width] duration-700 ease-in-out delay-[600ms] top-3/4 ${active ? 'w-full bg-primary' : ''}
         before:block before:content-[""] before:absolute before:-top-[5%] before:-left-[5%] before:-z-1 before:h-110% before:w-110% bg-primary'`}></div>
       </div>
-      <ul className='block absolute z-2 left-[55%] top-[19%] m-0 p-0 list-none text-white '>
+      <ul className='block absolute z-2 left-[55%] top-[19%] m-0 p-0 list-none text-white max-xl:w-full'>
         {menuItems.map((item, index) => (
           <li key={index} className='overflow-hidden list-item group'>
-            <a href="#" className={`inline-block text-7xl uppercase tracking-tight leading-snug transition-transform duration-[800ms] ease-in-out ${active ? ' translate-y-0 delay-[500ms]' : 'translate-y-full'}`}>
-              <span className='group-hover:block group-hover:transition-all group-hover:duration-500 group-hover:delay-300 group-hover:ease-in-out group-hover:content-[""] group-hover:translate-x-20'>
+            <a href="#" className={`inline-block text-7xl max-xl:text-4xl max-xl:leading-relaxed  uppercase tracking-tight leading-snug transition-transform duration-[800ms] ease-in-out ${active ? ' translate-y-0 delay-[500ms]' : 'translate-y-full'}`}>
+              <span className='group-hover:block group-hover:transition-all group-hover:duration-500 group-hover:delay-300 group-hover:ease-in-out group-hover:content-[""] group-hover:translate-x-20 group-hover:max-xl:translate-x-10'>
                 {/* <em className='block content-[""] absolute left-0 top-1/2 z-10 w-16 h-1.5 origin-right bg-white scale-x-0 duration-300 ease-in-out
                 before:w-1.5 before:h-10 before:origin-[0%_100%] before:-rotate-90 before:content-[""] before:absolute before:-right-1 before:bottom-0 before:transition-transform before:bg-white before:duration-500 before:z-20
                 after:w-10 after:h-1 after:origin-[100%_0%] after:rotate-0 after:content-[""] after:transition-transform after:-right-1 after:bottom-0 after:-scale-x-1 after:bg-white after:duration-500 after:z-20'></em> */}
                 {item.title}
               </span>
-              <em className='group-hover:block group-hover:content-[""] group-hover:absolute group-hover:left-0 group-hover:top-2/3 group-hover:delay-500 group-hover:z-10 group-hover:w-16 group-hover:h-1.5 group-hover:origin-right group-hover:bg-white group-hover:scale-x-1 group-hover:duration-300 group-hover:delay-00 group-hover:ease-in-out'></em>
+              <em className='group-hover:block group-hover:content-[""] group-hover:absolute group-hover:left-0 group-hover:top-1/2 group-hover:delay-500 group-hover:z-10 group-hover:w-16 group-hover:max-xl:w-8 group-hover:h-1.5 group-hover:origin-right group-hover:bg-white group-hover:scale-x-1 group-hover:duration-300 '>{/* group-hover:before:delay-700 group-hover:ease-in-out group-hover:before:w-1.5 before:h-10 group-hover:before:origin-[0%_100%] group-hover:before:-rotate-45 group-hover:before:content-[""] group-hover:before:absolute group-hover:before:left-8 group-hover:before:bottom-0 group-hover:before:transition-transform group-hover:before:bg-white group-hover:before:duration-500 group-hover:before:z-20
+              group-hover:after:w-10 group-hover:after:h-1 group-hover:after:origin-[100%_0%] group-hover:after:rotate-45 group-hover:after:content-[""] group-hover:after:transition-transform group-hover:after:-right-1 group-hover:after:bottom-0 group-hover:after:-scale-x-1 group-hover:after:bg-white group-hover:after:duration-500 group-hover:after:z-20 */}</em>
               </a>
           </li>
         ))}
@@ -124,8 +126,8 @@ const Header = () => {
       <div className='block'> {/* wrapper */}
         <div className='flex relative items-center w-full h-20 p-[0.12%] xl:p-[0.5%]'>
           {/* logo */}
-          <div>
-            <a href="/">Logo</a>
+          <div className='absolute top-1/2 left-52 z-50 -translate-y-1/2 w-40 h-7 max-xl:left-6 transition-all duration-300 ease-in-out'>
+            <a href="/"><img src={Logo} alt="logo" /></a>
           </div>
 
           {/* 상단메뉴 */}
